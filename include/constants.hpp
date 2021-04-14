7ca9e3ad3752730543ed113cdd7aea2cc843e85f
Govind Chari <govindchari1@gmail.com>
2021-04-14 15:42:15 -0400

    Enabled c++14

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,6 +1,6 @@
 namespace mprop {
-    static  double mass = 1.0;
-    static  double I1 = 0.1;
-    static  double I2 = 0.1;
-    static  double I3 = 0.1;
+    static constexpr double mass = 1.0;
+    static constexpr double I1 = 0.1;
+    static constexpr double I2 = 0.1;
+    static constexpr double I3 = 0.1;
 }
