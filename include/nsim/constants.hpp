26e8513b11633b6c28974d36b2763c78b72ad302
Govind Chari <govindchari1@gmail.com>
2021-04-19 02:53:17 -0400

    Added Isp, g0, and number of 3DOF and 6DOF states

--- include/nsim/constants.hpp
+++ include/nsim/constants.hpp
@@ -1,15 +1,20 @@
 #pragma once
-namespace mprop {
-static constexpr double mass = 1.0;
+namespace vprop {
+static constexpr double m_dry = 1.0;
+static constexpr double m_fuel = 1.0;
 static constexpr double I1 = 0.1;
 static constexpr double I2 = 0.1;
 static constexpr double I3 = 0.1;
-} // namespace mprop
+static constexpr double Isp = 250.0;
+} // namespace vprop
 
 namespace sim {
 static constexpr double h = 0.1;
+static constexpr int num_states_6DOF = 13;
+static constexpr int num_states_3DOF = 6;
 } // namespace sim
 
 namespace environment {
 static constexpr double g = 9.81;
+static constexpr double g0 = 9.81;
 } // namespace environment
