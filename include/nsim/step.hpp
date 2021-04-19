cc8dbacd523714cd096b3dbf9cb4a45717262ed9
Govind Chari <govindchari1@gmail.com>
2021-04-19 19:09:29 -0400

    Remove h as parameter

--- include/nsim/step.hpp
+++ include/nsim/step.hpp
@@ -2,16 +2,16 @@
 #include "Eigen/Dense"
 using namespace Eigen;
 
-// Uses RK4 to step the simulation forward in time by h
+// Uses RK4 to step the simulation forward in time by sim::h
 template <typename T>
 T step(T (*f)(T const &state, const Vector3d &force, const Vector3d &moment, const double &thrust),
-       const T &x, const double &h, const Vector3d &F, const Vector3d &M, const double &Th) {
+       const T &x, const Vector3d &F, const Vector3d &M, const double &Th) {
   static constexpr double half = 0.5;
   static constexpr double two = 2.0;
   static constexpr double sixth = 1.0 / 6.0;
   auto k1 = f(x, F, M, Th);
-  auto k2 = f(x + half * h * k1, F, M, Th);
-  auto k3 = f(x + half * h * k2, F, M, Th);
-  auto k4 = f(x + h * k3, F, M, Th);
-  return x + (sixth)*h * (k1 + two * k2 + two * k3 + k4);
+  auto k2 = f(x + half * sim::h * k1, F, M, Th);
+  auto k3 = f(x + half * sim::h * k2, F, M, Th);
+  auto k4 = f(x + sim::h * k3, F, M, Th);
+  return x + (sixth)* sim::h * (k1 + two * k2 + two * k3 + k4);
 }
