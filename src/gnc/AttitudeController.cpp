64906fc7bf8a269dbd475eef59968e34e747dc11
Govind Chari <govindchari1@gmail.com>
2021-04-21 17:39:38 -0400

    Fixed signs in ThrustAllocator and AttitudeController

--- src/gnc/AttitudeController.cpp
+++ src/gnc/AttitudeController.cpp
@@ -5,7 +5,10 @@ Eigen::Vector3d AttitudeController(const Eigen::Vector4d &q, const Eigen::Vector
                                    Eigen::Vector3d &q_int) {
   Eigen::Vector3d M_des;
   Eigen::Vector3d v = q.block<3, 1>(1, 0);
-  M_des = -Attitude::K_p * v - Attitude::K_i * q_int - Attitude::K_d * w;
+  
+  // The signs are pluses because the orientation quaternion indicates the direction to 
+  //rotate to go from body to inertial
+  M_des = Attitude::K_p * v + Attitude::K_i * q_int + Attitude::K_d * w;
 
   // Update q_int
   q_int += v * FlightComputer::dt;
--- src/gnc/ThrustAllocator.cpp
+++ src/gnc/ThrustAllocator.cpp
@@ -1,16 +1,16 @@
 #include "gnc/ThrustAllocator.hpp"
 #include <iostream>
 
-void ThrustAllocator(Vector3d &F_des, Vector3d &M_des, EngineActuation &actuation) {
+void ThrustAllocator(Vector3d &T_des, Vector3d &M_des, EngineActuation &actuation) {
   Vector3d T_b; // Thrust in the body frame
   Vector3d T_g; // Thrust in the gimbal frame
   Vector3d nhat;
   double theta;
 
-  T_g << 0, 0, F_des.norm();
-  double T_x = M_des(1, 0) / vprop::l;
-  double T_y = -M_des(0, 0) / vprop::l;
-  double T_z = sqrt(pow(T_x,2) + pow(T_y,2));
+  T_g << 0, 0, T_des.norm();
+  double T_x = -M_des(1, 0) / vprop::l;
+  double T_y = M_des(0, 0) / vprop::l;
+  double T_z = sqrt(pow(T_des.norm(), 2) - pow(T_x,2) - pow(T_y,2));
   T_b << T_x, T_y, T_z;
   std::cout << "T_body:" << std::endl;
   std::cout << T_b << std::endl;
@@ -18,9 +18,9 @@ void ThrustAllocator(Vector3d &F_des, Vector3d &M_des, EngineActuation &actuatio
 
   nhat = cross(T_g, T_b);
   nhat.normalize();
-  theta = asin(dot(T_g, T_b) / pow(F_des.norm(), 2));
+  theta = acos(dot(T_g, T_b) / pow(T_des.norm(), 2));
 
-  actuation.throttle = F_des.norm();
+  actuation.throttle = T_des.norm();
   actuation.axis = nhat;
   actuation.angle = theta;
 }
