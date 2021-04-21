66b1fcc9ed46e8ba041dc4c3d2e0054fa8ef008e
Govind Chari <govindchari1@gmail.com>
2021-04-21 15:38:20 -0400

    Added ThrustAllocator

---
+++ include/gnc/ThrustAllocator.hpp
@@ -0,0 +1,21 @@
+#include "Eigen/Dense"
+#include "gnc/constants.hpp"
+#include "gnc/utilities.hpp"
+using namespace Eigen;
+
+struct EngineActuation {
+  double throttle;
+  Vector3d axis;
+  double angle;
+};
+
+/**
+ * @brief Computes the throttle and TVC angle from the desired thrust vector and
+ *  desired moment. The actuation struct is mutated.
+ * 
+ * @param T_des Desired thrust vector in inertial coordinates
+ * @param M_des Desired moment on the vehicle
+ * @param actuation Contains the thrust magnitude (throttle) and the axis and 
+ * angle that the gimbal will rotate with respect to the body frame
+ */
+void ThrustAllocator(Vector3d &T_des, Vector3d &M_des, EngineActuation &actuation);
---
+++ src/gnc/ThrustAllocator.cpp
@@ -0,0 +1,25 @@
+#include "gnc/ThrustAllocator.hpp"
+#include <iostream>
+
+void ThrustAllocator(Vector3d &F_des, Vector3d &M_des, EngineActuation &actuation) {
+  Vector3d T_b; // Thrust in the body frame
+  Vector3d T_g; // Thrust in the gimbal frame
+  Vector3d nhat;
+  double theta;
+
+  T_g << 0, 0, F_des.norm();
+  double T_x = M_des(1, 0) / vprop::l;
+  double T_y = -M_des(0, 0) / vprop::l;
+  double T_z = sqrt(pow(T_x,2) + pow(T_y,2));
+  T_b << T_x, T_y, T_z;
+  std::cout << "T_body:" << std::endl;
+  std::cout << T_b << std::endl;
+  std::cout << "" << std::endl;
+
+  nhat = cross(T_g, T_b) / cross(T_g, T_b).norm();
+  theta = asin(dot(T_g, T_b) / pow(F_des.norm(), 2));
+
+  actuation.throttle = F_des.norm();
+  actuation.axis = nhat;
+  actuation.angle = theta;
+}
