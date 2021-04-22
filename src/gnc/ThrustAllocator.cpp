d335fda0707bfebd4c5ecc9d682a20e8c4f9feb1
Govind Chari <govindchari1@gmail.com>
2021-04-22 00:54:51 -0400

    Removed debugging and renamed thrust vectors

--- src/gnc/ThrustAllocator.cpp
+++ src/gnc/ThrustAllocator.cpp
@@ -1,24 +1,20 @@
 #include "gnc/ThrustAllocator.hpp"
-#include <iostream>
 
 void ThrustAllocator(Vector3d &T_des, Vector3d &M_des, EngineActuation &actuation) {
-  Vector3d T_b; // Thrust in the body frame
-  Vector3d T_g; // Thrust in the gimbal frame
+  Vector3d T_body; // Thrust in the body frame
+  Vector3d T_gimbal; // Thrust in the gimbal frame
   Vector3d nhat;
   double theta;
 
-  T_g << 0, 0, T_des.norm();
+  T_gimbal << 0, 0, T_des.norm();
   double T_x = -M_des(1, 0) / vprop::l;
   double T_y = M_des(0, 0) / vprop::l;
   double T_z = sqrt(pow(T_des.norm(), 2) - pow(T_x,2) - pow(T_y,2));
-  T_b << T_x, T_y, T_z;
-  std::cout << "T_body:" << std::endl;
-  std::cout << T_b << std::endl;
-  std::cout << "" << std::endl;
+  T_body << T_x, T_y, T_z;
 
-  nhat = cross(T_g, T_b);
+  nhat = cross(T_gimbal, T_body);
   nhat.normalize();
-  theta = acos(dot(T_g, T_b) / pow(T_des.norm(), 2));
+  theta = acos(dot(T_gimbal, T_body) / pow(T_des.norm(), 2));
 
   actuation.throttle = T_des.norm();
   actuation.axis = nhat;
