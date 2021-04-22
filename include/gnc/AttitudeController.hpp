fe2d8f6b877ab77fe7a86d8af989d8e13e73c9a0
Govind Chari <govindchari1@gmail.com>
2021-04-22 00:55:29 -0400

    Fixed attitude control to point along T_des

--- include/gnc/AttitudeController.hpp
+++ include/gnc/AttitudeController.hpp
@@ -1,13 +1,17 @@
 #pragma once
 #include "Eigen/Dense"
+#include "gnc/utilities.hpp"
 using namespace Eigen;
+
 /**
- * @brief Computes the desired moment on the vehicle and updates q_int
- *
- * @param q Vehicle orientation quaternion
- * @param w Vehicle Angular Velocity
- * @param q_int Integral of the vector portion of the orientation quaternion
- * @return Desired moment on the vehicle
+ * @brief Computes the desired moment on the vehicle in order to have the vehicle
+ * align with the desired thrust vector
+ * 
+ * @param T_des Desired thrust vector in an inertial frame
+ * @param q Orientation quaternion
+ * @param w Vehicle's angualar rate
+ * @param q_int Integrated error of quaternion
+ * @return Vector3d 
  */
-Vector3d AttitudeController(const Vector4d &q, const Vector3d &w,
+Vector3d AttitudeController(const Vector3d &T_des, const Vector4d &q, const Vector3d &w,
                             Vector3d &q_int);
