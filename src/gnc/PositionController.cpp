1d92324061f37f11804fe0223a2fd863da881a50
Govind Chari <govindchari1@gmail.com>
2021-04-23 15:24:24 -0400

    Disallowed negative z thrusts

--- src/gnc/PositionController.cpp
+++ src/gnc/PositionController.cpp
@@ -6,6 +6,9 @@ Vector3d PositionController(const Vector3d &pos, const Vector3d &vel,
   Vector3d T_des;
   T_des = -Position::K_p * pos - Position::K_i * pos_int - Position::K_d * vel;
 
+  if (T_des(2,0)<0){
+    T_des(2,0)=0;
+  }
   // Update pos_int
   pos_int += pos * FlightComputer::dt;
   return T_des;
