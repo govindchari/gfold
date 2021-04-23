1cbbb2948b08061033625759df8d79d37c4d0e7e
Govind Chari <govindchari1@gmail.com>
2021-04-23 15:18:10 -0400

    Updated SaveData to push out more info

--- include/nsim/SaveData.hpp
+++ include/nsim/SaveData.hpp
@@ -4,6 +4,4 @@
 #include <fstream>
 
 // Saves Data to .csv
-void SaveData(const std::vector<Eigen::Matrix<double, sim::num_states_3DOF, 1>> &data);
-
-void SaveData(const std::vector<Eigen::Matrix<double, sim::num_states_6DOF, 1>> &data);
+void SaveData(const std::vector<Eigen::Matrix<double, 35, 1>> &data);
--- src/nsim/SaveData.cpp
+++ src/nsim/SaveData.cpp
@@ -1,33 +1,16 @@
 #include "nsim/SaveData.hpp"
 #include "nsim/constants.hpp"
 
-void SaveData(const std::vector<Eigen::Matrix<double, sim::num_states_6DOF, 1>> &data) {
+void SaveData(const std::vector<Eigen::Matrix<double, sim::num_states_6DOF + 21, 1>> &data) {
   std::ofstream output_file("./results/data.csv");
   int j = 1;
   std::for_each(data.begin(), data.end(), [&](auto const &x) {
     output_file << j * sim::h;
     output_file << ",";
-    for (int i = 0; i < sim::num_states_6DOF; i++) {
+    for (int i = 0; i < sim::num_states_6DOF + 21; i++) {
       output_file << x(i, 0);
       output_file << ",";
-      if (i == sim::num_states_6DOF - 1) {
-        output_file << std::endl;
-      }
-    }
-    j++;
-  });
-}
-
-void SaveData(const std::vector<Eigen::Matrix<double, sim::num_states_3DOF, 1>> &data) {
-  std::ofstream output_file("./results/data.csv");
-  int j = 1;
-  std::for_each(data.begin(), data.end(), [&](auto const &x) {
-    output_file << j * sim::h;
-    output_file << ",";
-    for (int i = 0; i < sim::num_states_3DOF; i++) {
-      output_file << x(i, 0);
-      output_file << ",";
-      if (i == sim::num_states_3DOF - 1) {
+      if (i == sim::num_states_6DOF + 21 - 1) {
         output_file << std::endl;
       }
     }
