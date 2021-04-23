fb299341d9d1a73e75e33c60847e9d7c235c1029
Govind Chari <govindchari1@gmail.com>
2021-04-23 17:35:05 -0400

    Added thrust offset to compensate for weight

--- src/gnc/PositionController.cpp
+++ src/gnc/PositionController.cpp
@@ -4,7 +4,10 @@
 Vector3d PositionController(const Vector3d &pos, const Vector3d &vel,
                             Vector3d &pos_int) {
   Vector3d T_des;
+  Vector3d weight;
+  weight << 0,0,(vprop::m_dry+vprop::m_fuel)*environment::g;
   T_des = -Position::K_p * pos - Position::K_i * pos_int - Position::K_d * vel;
+  T_des += weight;
 
   if (T_des(2,0)<0){
     T_des(2,0)=0;
