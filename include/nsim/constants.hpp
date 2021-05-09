c956e960dc7ca14c983ecebe8316efdb28a0268a
Govind Chari <govindchari1@gmail.com>
2021-05-08 23:49:55 -0400

    Changed to 1Hz

--- include/nsim/constants.hpp
+++ include/nsim/constants.hpp
@@ -1,6 +1,6 @@
 #pragma once
 namespace sim {
-static constexpr double h = 0.01;
+static constexpr double h = 1;
 static constexpr int num_states_6DOF = 14;
 static constexpr int num_states_3DOF = 7;
 } // namespace sim
