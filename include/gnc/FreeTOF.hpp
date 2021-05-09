db70419cb888c659a3231be233b7ce908e6d34e6
Govind Chari <govindchari1@gmail.com>
2021-05-09 01:34:17 -0400

    Warmstart TOF search

--- include/gnc/FreeTOF.hpp
+++ include/gnc/FreeTOF.hpp
@@ -1,16 +1,16 @@
 #pragma once
 #include "gnc/FixedTOF.hpp"
 
-
 /**
- * @brief Computes the free time of flight optimal trajectory. Currently just takes the
- * minimum time of flight feasible solution.
- * 
+ * @brief Computes the free time of flight optimal trajectory. Currently just
+ * takes the minimum time of flight feasible solution.
+ *
  * @param r0 Initial position
  * @param v0 Initial velocity
  * @param m0 Initial mass
- * @param data Struct containing trajectory, control inputs, and terminal mass 
+ * @param tof_guess A guess for the optimal time-of-flight (previous optimal tof minus dt)
+ * @param data Struct containing trajectory, control inputs, and terminal mass
  * @return First thrust command
  */
 Vector3d FreeTOF(const Vector3d &r0, const Vector3d &v0, const double &m0,
-             FixedTOFData &data);
+                double &tof_guess, FixedTOFData &data);
--- src/gnc/FreeTOF.cpp
+++ src/gnc/FreeTOF.cpp
@@ -1,14 +1,20 @@
 #include "gnc/FreeTOF.hpp"
 #include <iostream>
 
-Vector3d FreeTOF(const Vector3d &r0, const Vector3d &v0, const double &m0, FixedTOFData &data) {
+Vector3d FreeTOF(const Vector3d &r0, const Vector3d &v0, const double &m0, double &tof_guess, FixedTOFData &data) {
     int t_min = (int) (massprop::m_dry) * v0.norm() / propulsion::T_max;
     int t_max = (int) (m0 - massprop::m_dry) / (propulsion::alph * propulsion::T_min);
     double i = t_min;
+    if (tof_guess < t_max) {
+        i = tof_guess-FlightComputer::dt;
+    }
+
     while (i < t_max){
         FixedTOF(r0, v0, i, m0, data);
+        //std::cout << i << std::endl;
         if((data.m_final < massprop::m_dry + massprop::m_fuel)) {
             std::cout << i << std::endl;
+            tof_guess = i - FlightComputer::dt;
             return m0 * data.u_sol.col(0);
         }
         i+=FlightComputer::dt;
