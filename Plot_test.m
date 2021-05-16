a0cbae7aa37ff886cb41a3aace7f49ae8792e40a
Govind Chari <govindchari1@gmail.com>
2021-05-16 00:09:41 -0400

    Updated PlotTest

--- Plot_test.m
+++ Plot_test.m
@@ -1,15 +1,21 @@
-close all
+clc
 u=csvread('u');
 pos=csvread('pos');
+z=csvread('z');
+
+m_actual=exp(z);
 
 limx = 3000;
 limy = 2500;
 limz = 3500;
 
-figure(3)
+figure(20)
 plot3(pos(1,:), pos(2,:), pos(3,:), 'linewidth',2)
 hold on
 quiver3(pos(1,:), pos(2,:), pos(3,:), u(1,:), u(2,:), u(3,:))
+hold on
+plot3(r(:,1),r(:,2),r(:,3),'Linewidth',2)
+grid on
 
 plot(pos(1,:), pos(2,:),'color','black')
 plot3(limx*ones(1,length(pos)),pos(2,:),pos(3,:),'color','black')
@@ -19,9 +25,13 @@ xlabel('x (m)')
 ylabel('y (m)')
 zlabel('z (m)')
 
+xlim([-2500 0])
+ylim([-100 700])
+zlim([0 2000])
+
 title('Out of Plane Divert Maneuver')
 
-%xlim([0 limx])
-%ylim([0 limy])
-%zlim([0 limz])
+figure(21)
+plot(m_actual)
+
 grid on
