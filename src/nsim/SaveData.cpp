006acf9f8a218c7c477905a9426906833d8ff7eb
Govind Chari <govindchari1@gmail.com>
2021-05-16 00:01:44 -0400

    Update SaveData

--- src/nsim/SaveData.cpp
+++ src/nsim/SaveData.cpp
@@ -1,16 +1,33 @@
 #include "nsim/SaveData.hpp"
-#include "nsim/constants.hpp"
+#include "gnc/constants.hpp"
 
-void SaveData(const std::vector<Eigen::Matrix<double, sim::num_states_6DOF + 21, 1>> &data) {
-  std::ofstream output_file("./results/data.csv");
+void SaveData(const std::vector<Eigen::Matrix<double, 15, 1>> &data) {
+  std::ofstream output_file("./results/fc_data.csv");
   int j = 1;
   std::for_each(data.begin(), data.end(), [&](auto const &x) {
-    output_file << j * sim::h;
+    output_file << FlightComputer::dt * j;
     output_file << ",";
-    for (int i = 0; i < sim::num_states_6DOF + 21; i++) {
+    for (int i = 0; i < 15; i++) {
       output_file << x(i, 0);
       output_file << ",";
-      if (i == sim::num_states_6DOF + 21 - 1) {
+      if (i == 14) {
+        output_file << std::endl;
+      }
+    }
+    j++;
+  });
+}
+
+void SaveData(const std::vector<Eigen::Matrix<double, 20, 1>> &data) {
+  std::ofstream output_file("./results/sim_data.csv");
+  int j = 1;
+  std::for_each(data.begin(), data.end(), [&](auto const &x) {
+    output_file << sim::h * j;
+    output_file << ",";
+    for (int i = 0; i < 20; i++) {
+      output_file << x(i, 0);
+      output_file << ",";
+      if (i == 19) {
         output_file << std::endl;
       }
     }
