b75d2cc562c3c5809c3346497da1b622a330fa15
Govind Chari <govindchari1@gmail.com>
2021-05-16 00:03:18 -0400

    Updated SaveData

--- include/nsim/SaveData.hpp
+++ include/nsim/SaveData.hpp
@@ -4,4 +4,5 @@
 #include <fstream>
 
 // Saves Data to .csv
-void SaveData(const std::vector<Eigen::Matrix<double, 35, 1>> &data);
+void SaveData(const std::vector<Eigen::Matrix<double, 15, 1>> &data);
+void SaveData(const std::vector<Eigen::Matrix<double, 20, 1>> &data);
