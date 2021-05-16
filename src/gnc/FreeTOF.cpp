3b50b6a59072d17c7d0be54f73a8576b707357da
Govind Chari <govindchari1@gmail.com>
2021-05-16 00:07:21 -0400

    Warmstart Solver

--- src/gnc/FreeTOF.cpp
+++ src/gnc/FreeTOF.cpp
@@ -6,12 +6,14 @@ Vector3d FreeTOF(const Vector3d &r0, const Vector3d &v0, const double &m0, doubl
     int t_max = (int) (m0 - massprop::m_dry) / (propulsion::alph * propulsion::T_min);
     double i = t_min;
     if (tof_guess < t_max) {
-        i = tof_guess-FlightComputer::dt;
+        i = tof_guess - 10.0 * FlightComputer::dt;
+        if (tof_guess < 15.0 * FlightComputer::dt) {
+            i=0.0;
+        }
     }
 
     while (i < t_max){
         FixedTOF(r0, v0, i, m0, data);
-        //std::cout << i << std::endl;
         if((data.m_final < massprop::m_dry + massprop::m_fuel)) {
             std::cout << i << std::endl;
             tof_guess = i - FlightComputer::dt;
