ec215169eb6b74c5c218c6626d33d25f5771d566
Govind Chari <govindchari1@gmail.com>
2021-05-18 01:38:35 -0400

    Included processnoise cov

--- include/nsim/constants.hpp
+++ include/nsim/constants.hpp
@@ -1,5 +1,13 @@
 #pragma once
 namespace sim {
-static constexpr double h = 0.001;
+static constexpr double h = 0.1;
 static constexpr int num_states_6DOF = 14;
 } // namespace sim
+
+namespace process_noise {
+static constexpr double sig_Fx = 10000;
+static constexpr double sig_Fy = 10000;
+static constexpr double sig_Fz = 10000;
+static constexpr double sig_Mx = 10000;
+static constexpr double sig_My = 10000;
+} // namespace process_noise
